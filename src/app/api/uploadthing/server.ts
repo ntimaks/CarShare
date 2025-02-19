e152a4c88707591bc0b4c717f8107a12bf3213b2
ntimaks <ntimaks27@gmail.com>
2025-02-19 20:40:48 +0200

    fix build error

--- src/app/api/uploadthing/server.ts
+++ src/app/api/uploadthing/server.ts
@@ -1,9 +1,13 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
 const f = createUploadthing();
-export const uploadRouter = {};
-// ...
-f({})
-    .middleware(({ req }) => {
-        //           ^? req: NextRequest
-        return {}
-    })
+
+export const uploadRouter: FileRouter = {
+    imageUploader: f({ image: { maxFileSize: "4MB" } })
+        .middleware(() => {
+            return { userId: "test" }; // Add any necessary auth/validation here
+        })
+        .onUploadComplete(({ metadata, file }) => {
+            console.log("Upload complete", file.url, metadata);
+        })
+};
