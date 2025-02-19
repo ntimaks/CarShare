669ce14c239577bd357d8476f16badfe295601b7
ntimaks <ntimaks27@gmail.com>
2025-02-19 21:27:50 +0200

    hopefully auth fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": ["error", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_",
+      }],
+      "@typescript-eslint/no-explicit-any": ["warn", {
+        "ignoreRestArgs": true
+      }],
+    },
+    files: ["**/*.ts", "**/*.tsx"],
+  },
 ];
 
 export default eslintConfig;
