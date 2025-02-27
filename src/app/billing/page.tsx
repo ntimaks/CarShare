95e26d1f605fc4483ac2bfba2d2edaad53e8ac7f
ntimaks <ntimaks27@gmail.com>
2025-02-27 13:12:53 +0200

    billing page

---
+++ src/app/billing/page.tsx
@@ -0,0 +1,23 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Billing() {
+    const supabase = createClient()
+    const { data: { user } } = await (await supabase).auth.getUser()
+
+    if (!user) {
+        redirect("/login")
+    }
+
+    return (
+        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Billing</CardTitle>
+                    <CardDescription>Manage your billing information</CardDescription>
+                </CardHeader>
+            </Card>
+        </section>
+    )
+}
